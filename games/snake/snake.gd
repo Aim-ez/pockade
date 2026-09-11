77757323e7cfa9d80b6441d1fc00a3b2d2ccd49e
Ezia <theaimeeschmidt@gmail.com>
2026-09-10 20:06:23 -0500

    Add Snake food and growth

--- games/snake/snake.gd
+++ games/snake/snake.gd
@@ -7,6 +7,7 @@ var snake = [
 	Vector2i(19, 15),
 	Vector2i(18, 15)
 ]
+var food = Vector2i(10, 10)
 var direction = Vector2i.RIGHT
 var move_timer = 0.0
 var move_delay = 0.15 # slow moving snake to start
@@ -20,6 +21,15 @@ func _draw():
 			Color.GREEN
 		)
 		
+		draw_rect(
+			Rect2(Vector2(food) * cell_size, Vector2(16, 16)),
+			Color.RED
+		)
+		
+func _ready():
+	spawn_food()
+	queue_redraw()
+	
 func _process(delta):
 	move_timer += delta
 
@@ -29,10 +39,15 @@ func _process(delta):
 		queue_redraw()
 		
 func move_snake():
+	var ate_food = snake[0] == food
 	for i in range(snake.size() - 1, 0, -1):
 		snake[i] = snake[i - 1]
 
 	snake[0] += direction
+	
+	if ate_food:
+		snake.append(snake[-1])
+		spawn_food()
 	
 func _input(event):
 	if event is InputEventKey and event.pressed:
@@ -48,3 +63,9 @@ func _input(event):
 func change_direction(new_direction):
 	if new_direction != -direction:
 		direction = new_direction
+
+func spawn_food():
+	food = Vector2i(
+		randi_range(0,39),
+		randi_range(0,29)
+	)
